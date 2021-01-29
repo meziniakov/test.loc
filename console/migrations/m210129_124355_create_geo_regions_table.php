db6a0e41ebf186aee66f69f52f98ebf82b271708
mezin <z2941@ya.ru>
2021-01-29 17:27:44 +0300

    .

--- console/migrations/m210129_124355_create_geo_regions_table.php
+++ console/migrations/m210129_124355_create_geo_regions_table.php
@@ -19,10 +19,10 @@ class m210129_124355_create_geo_regions_table extends Migration
             'district_id' => $this->integer(11)->unsigned()->notNull()->defaultValue(0),
             'name' => $this->string(255)->notNull(),
         ]);
-        $this->batchInsert('{{%geo_regions}}', ['id', 'district_id', 'name'], [
-            [2, 3, 'Адыгея'],
-            [3, 6, 'Алтай'],
-        ]);
+        // $this->batchInsert('{{%geo_regions}}', ['id', 'district_id', 'name'], [
+        //     [2, 3, 'Адыгея'],
+        //     [3, 6, 'Алтай'],
+        // ]);
         // }
     }
 
