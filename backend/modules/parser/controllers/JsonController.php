8ef887d0e8741158cbd538530f0680752c4958ff
mezin <z2941@ya.ru>
2021-08-16 23:33:49 +0300

    fix

--- backend/modules/parser/controllers/JsonController.php
+++ backend/modules/parser/controllers/JsonController.php
@@ -81,10 +81,6 @@ class JsonController extends Controller
                         'object' => $object,
                         'pathinfo' => pathinfo($object->image->url),
                     ]));
-                    $place->title = $object->name;
-                    $place->text = $object->description;            
-                    $place->slug = '';
-                    $place->save();
                     $countCreate++;
                 }
             }
