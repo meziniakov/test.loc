495dc7518cd673e1c89a0923fcb0f8ad158581eb
mezin <z2941@ya.ru>
2021-08-26 22:05:53 +0300

    add console controller updatePlaceJob

---
+++ console/controllers/TestController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace console\controllers;
+
+use backend\jobs\CreatePlaceJob;
+use backend\jobs\UpdatePlaceJob;
+use common\models\Place;
+use Yii;
+use yii\console\Controller;
+use yii\helpers\Console;
+use yii\helpers\Json;
+
+class TestController extends Controller
+{
+    public function actionTest()
+    {
+        $path = '/var/www/yii2zif/test.loc/storage/json/events/museums.json';
+        $json = file_get_contents($path, true);
+        $array = Json::decode($json, false);
+
+        $countCreate = 0;
+        $countUpdate = 0;
+        foreach ($array as $object) {
+            $object = $object->data->general;
+            if (Place::findOne(['title' => $object->name])) {
+                Yii::$app->queue->push(new UpdatePlaceJob([
+                    'object' => $object,
+                ]));
+                $countUpdate++;
+            } else {
+                Yii::$app->queue->push(new CreatePlaceJob([
+                    'object' => $object,
+                    'pathinfo' => pathinfo($object->image->url),
+                ]));
+                $countCreate++;
+            }
+        }
+    }
+}
