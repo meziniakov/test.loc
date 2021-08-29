f62e55ba6ffeb220c97d8845787b5a6025c1d364
mezin <z2941@ya.ru>
2021-08-29 14:31:35 +0300

    fix

--- backend/jobs/UpdatePlaceJob.php
+++ backend/jobs/UpdatePlaceJob.php
@@ -11,13 +11,10 @@ use common\models\Region;
 class UpdatePlaceJob extends BaseObject implements \yii\queue\JobInterface
 {
     public $object;
-    public $place;
 
     public function execute($queue)
-    {
-        
+    {        
         $object = $this->object;
-        // $place = $this->place;
         $place = Place::findOne(['title' => $object->name]);
 
         $place->title = $object->name;
@@ -25,23 +22,30 @@ class UpdatePlaceJob extends BaseObject implements \yii\queue\JobInterface
         $place->status = Place::STATUS_UPDATED;
         $place->text = $object->description;
         $place->address = $object->address->fullAddress;
-        $place->street = $object->address->street;
-        $place->street_comment = $object->address->fiasStreetId;
+        if(isset($object->address->street)){
+            $place->street = $object->address->street;
+        }
 
         $dadata = new \Dadata\DadataClient(env('DADATA_TOKEN'), env('DADATA_SECRET'));
         if(isset($object->address->fiasHouseId)) {
             $response = $dadata->findById("address", $object->address->fiasHouseId);
+            $place->street_comment = $object->address->fiasHouseId;
+        } elseif(isset($object->address->fiasStreetId)) {
+            $response = $dadata->findById("address", $object->address->fiasStreetId);
+            $place->street_comment = $object->address->fiasStreetId;
+        } elseif(isset($object->address->fiasCityId)) {
+            $response = $dadata->findById("address", $object->address->fiasCityId);
+            $place->street_comment = $object->address->fiasStreetId;
         } else {
-            $response = $dadata->findById("address", $object->address->fiasStreetId);                        
+            $response = $dadata->findById("address", $object->address->fiasSettlementId);
+            $place->street_comment = $object->address->fiasSettlementId;
         }
 
         if(!$federal_district = FederalDistrict::findOne(['name' => $response[0]['data']['federal_district']])) {
             $federal_district = new FederalDistrict();
             $federal_district->name = $response[0]['data']['federal_district'];
             $federal_district->country_id = 1;
-            if($federal_district->validate()){
-                $federal_district->save();
-            }
+            $federal_district->save();
         }
         if(!$region = Region::findOne(['name' => $response[0]['data']['region']])) {
             $region = new Region();
@@ -49,9 +53,7 @@ class UpdatePlaceJob extends BaseObject implements \yii\queue\JobInterface
             $region->name = $response[0]['data']['region'];
             $region->type = $response[0]['data']['region_type'];
             $region->type_full = $response[0]['data']['region_type_full'];
-            if($region->validate()){
-                $region->save();
-            }
+            $region->save();
         }
         if(!$area = Area::findOne(['name' => $response[0]['data']['area']])) {
             $area = new Area();
@@ -59,9 +61,8 @@ class UpdatePlaceJob extends BaseObject implements \yii\queue\JobInterface
             $area->name = $response[0]['data']['area'];
             $area->type = $response[0]['data']['area_type'];
             $area->type_full = $response[0]['data']['area_type_full'];
-            $area->validate();
             $area->save();
-    }
+        }
 
         if(!empty($response[0]['data']['city'])) {
             if ($city = City::findOne(['name' => $response[0]['data']['city']])) {
@@ -71,8 +72,6 @@ class UpdatePlaceJob extends BaseObject implements \yii\queue\JobInterface
                 $city = new City();
                 $city->name = $response[0]['data']['city'];
                 $city->area_id = $area->id;
-                // $city->url = $object->locale->sysName;
-                $city->validate();
                 $city->save();
                 $place->city_id = $city->id;
             }
@@ -80,7 +79,6 @@ class UpdatePlaceJob extends BaseObject implements \yii\queue\JobInterface
             $city = new City();
             $city->name = $response[0]['data']['settlement_with_type'];
             $city->area_id = $area->id;
-            // $city->url = $object->locale->sysName;
             $city->save();
             $place->city_id = $city->id;
         }
